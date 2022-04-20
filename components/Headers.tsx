cf7d59e30cea9a6ade72af5c41bc49cc3d9bce08
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-04-20 23:40:02 +0100

    header component responsiveness

--- components/Headers.tsx
+++ components/Headers.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import { BellIcon, SearchIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 
 const Headers = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
+
   return (
-    <header>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img 
           src="https://rb.gy/ulxxee"
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,27 @@ import Headers from '../components/Headers'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]'>
       <Head>
-        <title>Netflix</title>
+        <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      Let's build Netflix
+
       <Headers />
+
+      <main>
+        {/* Banner */}
+        <section>
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+          {/* Row */}
+        </section>
+      </main>
       
     </div>
   )
