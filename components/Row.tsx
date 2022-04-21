fc04b2a91c8c22cd306ca3b0704f03f496944dad
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-04-21 13:57:16 +0100

    chore: row containers

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,6 +1,20 @@
-const Row = () => {
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
+import { Movie } from "../typings";
+
+interface Props {
+  title: string;
+  movies: Movie[]
+}
+
+const Row = ( {title, movies} : Props) => {
   return (
-    <div>Row</div>
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2>{title}</h2>
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon  className="chevronIcons" />
+        <ChevronRightIcon className="chevronIcons" />
+      </div>
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -40,13 +40,17 @@ const Home = ({
       <Headers />
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         <Banner netflixOriginals={netflixOriginals} />
-        <section>
-          <Row />
-          <Row />
-          <Row />
-          <Row />
-          <Row />
-          <Row />
+        <section className='md:space-y-24'>
+        <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          {/* {list.length > 0 && <Row title="My List" movies={list} />} */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
       
