e66a1e0caa04d94dc87208b357f7fbc72b28c282
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-04-21 14:38:01 +0100

    chore: thumbnail component

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,5 +1,6 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/outline";
 import { Movie } from "../typings";
+import Thumbnail from "./Thumbnail";
 
 interface Props {
   title: string;
@@ -9,9 +10,16 @@ interface Props {
 const Row = ( {title, movies} : Props) => {
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
-      <h2>{title}</h2>
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">{title}</h2>
       <div className="group relative md:-ml-2">
         <ChevronLeftIcon  className="chevronIcons" />
+
+        <div className="flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2">
+          {movies.map(movie => (
+            <Thumbnail key={movie.id} movie={movie} />
+            ))}
+        </div>
+
         <ChevronRightIcon className="chevronIcons" />
       </div>
     </div>
