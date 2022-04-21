6a5f838c996b4970b6e62b85c8c1f3ffa8999d94
Anjola Adeuyi <57623705+anjola-adeuyi@users.noreply.github.com>
2022-04-21 11:06:03 +0100

    next image config

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image"
+import { useEffect, useState } from "react";
+import { baseUrl } from "../constants/movie";
 import { Movie } from "../typings"
 
 interface Props {
@@ -5,8 +8,18 @@ interface Props {
 }
 
 const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]);
+  }, [netflixOriginals])
+  
   return (
-    <div>Banner</div>
+    <div>
+      <div className="absolute top-0 left-0 h-[95vh] w-screen">
+        <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`} layout="fill" />
+      </div>
+    </div>
   )
 }
 
